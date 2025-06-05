0dd12c6d6f47dd32ebf4899a14889770519b9fe5
SEKOU TOURE <ahsekoutoure@gmail.com>
2025-06-05 05:42:17 -0700

    premier Exo en JavaScrit

---
+++ Exo1/index.js
@@ -0,0 +1,13 @@
+let hour = 11
+let name = "SEKOU"
+if (hour >=6 && hour <=12)
+    {
+    console.log(`Bonjour! ${name}`)
+}
+else if (hour >=13 && hour <=17)
+    {
+    console.log("Bon après midi " + name)
+}
+else{
+    console.log("Bonne Nuit! "+ name)
+}
